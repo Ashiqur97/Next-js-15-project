5e7e14d0312fefa979e3b6a99520177bbbb57aae
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-11-12 17:33:22 +0600

    code change

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,13 +1,9 @@
 import Form from "next/form";
+import SearchFormReset from "./SearchFormReset";
 
 const SearchForm = () => {
     const query = 'Test';
 
-    const reset = () => {
-        const form = document.querySelector(selectors: '.search-form') as HTMLFormElement;
-
-        if(form) form.reset();
-    }
   return (
     <Form action="/" scroll={false} className="search-form">
       <input 
@@ -17,11 +13,7 @@ const SearchForm = () => {
             placeholder="Search Startups"
       />
       <div className="flex gap-2">
-            {query && (
-                <button type="reset" onClick={reset}>
-
-                </button>
-            )}
+            {query && <SearchFormReset /> }
       </div>
     </Form>
   )
