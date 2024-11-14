a2a825bce76e33103d9aaf0a4bd9d38b36e1cf57
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-11-14 11:58:47 +0600

    code commit

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,8 +1,9 @@
 import {formatDate} from "@/lib/utils";
 import { EyeIcon, Link } from "lucide-react";
+import Image from "next/image";
 
 const StartupCard = ({post}: {post: StartupTypeCard}) => {
-  const {_createdAt,views,author: {_id:authorId,name},title,category_id,image} = post;
+  const {_createdAt,views,author: {_id:authorId,name},title,category_id,_id,image} = post;
 
   return (
     <li className="startup-card group">
@@ -18,12 +19,18 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
 
       <div className="flex-between mt-5 gap-5">
           <div className="flex-1">
-              <Link href={`/user/${post.author?._id}`}>
+              <Link href={`/user/${authorId}`}>
                   <p className="text-16-medium line-clamp-1">
                       {name}
                   </p>
               </Link>
+              <Link href={`/startup/${_id}`}>
+                  <h3 className="text-26-semibold line-camp">{title}</h3>
+              </Link>
           </div>
+            <Link href={`/user/${authorId}`}>
+              <Image src="https://placehold.co/600x400" alt="placeholder" width={48} height={40} className="rounded-full" />
+            </Link>          
       </div>
     </li>
   )
